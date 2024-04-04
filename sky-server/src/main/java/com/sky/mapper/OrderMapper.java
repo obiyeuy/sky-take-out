78ebf1515be39f062beeaf604cf698068cbb320e
obiyeuy <yueyibohitwh@126.com>
2024-04-04 17:19:09 +0800

    订单状态定时处理代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,8 +1,15 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
+import jdk.vm.ci.meta.Local;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * ClassName:OrderMapper
@@ -36,4 +43,44 @@ public interface OrderMapper {
      * @param orders
      */
     void update(Orders orders);
+
+    /**
+     * 分页条件查询并按下单时间排序
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+
+    /**
+     * 根据id查询订单
+     * @param id
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
+
+    /**
+     * 根据状态统计订单数量
+     * @param status
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
+
+    /**
+     * 为了规避微信支付所需要的更新订单数据语句
+     * @param orderStatus
+     * @param orderPaidStatus
+     * @param check_out_time
+     * @param id
+     */
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus},checkout_time = #{check_out_time} where id = #{id}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time,Long id);
+
+    /**
+     * 根据订单状态和下单时间来查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,26 @@
+package com.sky.task;
+
+/**
+ * ClassName:MyTask
+ * <p>
+ * Package:com.sky.task
+ * <p>
+ * Description:
+ * <p>
+ *
+ * @Author: yyb
+ * @Create: 2024/4/3-15:36
+ * @Version: v1.0
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义定时任务类
+ */
+@Component
+@Slf4j
+public class MyTask {
+
+}
